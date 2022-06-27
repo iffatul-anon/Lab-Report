eb3cb896eda459404c21f5d10b39cdb8687fa5af
Md. Iffatul Islam Anon <105257268+iffatul-anon@users.noreply.github.com>
2022-06-27 18:42:50 +0600

    Update 24.c

--- 24.c
+++ 24.c
@@ -2,22 +2,18 @@
 #include<stdio.h>
 
 int main(){
-    int n,m,sum_total=0,sum_first_last=0,reverse=0;
+    int n,m,sum=0,reverse=0;
     printf("Enter a number:");
     scanf("%d",&n);
 
-    m=n%10;
-    sum_first_last=m;
     while(n>0){
         m=n%10;
         n=n/10;
-        sum_total=sum_total+m;
+        sum=sum+m;
         reverse=(reverse*10)+m;
     }
-    sum_first_last=sum_first_last+m;
 
-    printf("Sum of digits = %d\n",sum_total);
-    printf("Sum of first and last digit = %d\n",sum_first_last);
+    printf("Sum of digits = %d\n",sum);
     printf("Reverse of the number = %d\n",reverse);
 
     return 0;
